f651895c873c4ab11916a23ef71723eda7e93d0c
agustinlinari <agustinlinari@gmail.com>
2017-09-10 19:41:07 -0300

    Pasar al siguiente link

--- app/src/main/java/com/tddp2/grupo2/linkup/activity/LinksFragment.java
+++ app/src/main/java/com/tddp2/grupo2/linkup/activity/LinksFragment.java
@@ -3,6 +3,7 @@ package com.tddp2.grupo2.linkup.activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,7 +14,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,28 +27,45 @@ import com.tddp2.grupo2.linkup.LinksView;
 import com.tddp2.grupo2.linkup.LoginActivity;
 import com.tddp2.grupo2.linkup.R;
 import com.tddp2.grupo2.linkup.controller.LinksController;
+import com.tddp2.grupo2.linkup.exception.MissingAgeException;
 import com.tddp2.grupo2.linkup.model.Links;
 import com.tddp2.grupo2.linkup.model.Profile;
 import com.tddp2.grupo2.linkup.service.factory.ServiceFactory;
+import com.tddp2.grupo2.linkup.utils.DateUtils;
+import com.tddp2.grupo2.linkup.utils.ImageUtils;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LinksFragment extends Fragment implements LinksView{
 
     private static final String TAG = "LinksFragment";
 
-    //private List<Candidate> candidates;
-    private Map<String, String> photos;
+    @BindView(R.id.linkImage)
+    ImageView imageViewLinkImage;
 
-    //private CandidateViewHolder candidateViewHolder;
-    private TextView emptyView;
+    @BindView(R.id.linkName)
+    TextView textViewLinkName;
+
+    @BindView(R.id.linkAge)
+    TextView textViewLinkAge;
+
+    @BindView(R.id.linkInterests)
+    TextView textViewLinkInterests;
+
+    @BindView(R.id.yesButton)
+    ImageButton buttonYes;
+
+    @BindView(R.id.noButton)
+    ImageButton buttonNo;
+
+    @BindView(R.id.reloadButton)
+    ImageButton reloadButton;
+
     private Context activity;
-    private LinearLayout linearLayoutHeaderProgress;
-    private ImageButton buttonYes;
-    private ImageButton buttonNo;
-    private ImageButton reloadButton;
-
     private LinksController controller;
     private Links links;
 
@@ -60,16 +80,35 @@ public class LinksFragment extends Fragment implements LinksView{
         View mainView = inflater.inflate(R.layout.fragment_links, container, false);
         mainView.setTag(TAG);
 
-        //candidateViewHolder = new CandidateViewHolder(mainView);
-      //  linearLayoutHeaderProgress = (LinearLayout) mainView.findViewById(R.id.linlaHeaderProgress);
+        ButterKnife.bind(this, mainView);
 
-        photos = new ConcurrentHashMap<>();
-
+        registerListeners();
         // ListView
         //emptyView = (TextView) mainView.findViewById(R.id.empty_view);
         controller.getLinks();
 
         return mainView;
+    }
+
+    private void registerListeners() {
+
+        buttonYes.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                controller.nextLink();
+            }
+        });
+
+        buttonNo.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                controller.previousLink();
+            }
+        });
     }
 
 
@@ -108,7 +147,17 @@ public class LinksFragment extends Fragment implements LinksView{
 
     @Override
     public void showLink(Profile profile) {
-
+        String image = profile.getImages().get(0).getImage();
+        Bitmap bitmap = ImageUtils.base64ToBitmap(image);
+        imageViewLinkImage.setImageBitmap(bitmap);
+        textViewLinkName.setText(profile.getFirstName());
+        String age = "";
+        try {
+            age = String.valueOf(DateUtils.getAgeFromBirthday(profile.getBirthday()));
+        } catch (MissingAgeException e) {
+        }
+        textViewLinkAge.setText(age);
+        textViewLinkInterests.setText(profile.getComments());
     }
 
     @Override
