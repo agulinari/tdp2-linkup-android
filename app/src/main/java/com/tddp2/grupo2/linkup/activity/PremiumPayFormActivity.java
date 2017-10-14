11cd5d5f9db8e87dcdc30ea1b7588c2134979776
Paula Saffioti <saffiotipaula@gmail.com>
2017-10-14 19:18:52 -0300

    pago con tarjeta de credito

--- app/src/main/java/com/tddp2/grupo2/linkup/activity/PremiumPayFormActivity.java
+++ app/src/main/java/com/tddp2/grupo2/linkup/activity/PremiumPayFormActivity.java
@@ -3,6 +3,7 @@ package com.tddp2.grupo2.linkup.activity;
 import android.content.BroadcastReceiver;
 import android.os.Bundle;
 import butterknife.ButterKnife;
+import com.braintreepayments.cardform.view.CardForm;
 import com.tddp2.grupo2.linkup.R;
 import com.tddp2.grupo2.linkup.infrastructure.messaging.Notification;
 
@@ -14,6 +15,15 @@ public class PremiumPayFormActivity extends BroadcastActivity {
         setContentView(R.layout.activity_premium_pay_form);
         ButterKnife.bind(this);
 
+        CardForm cardForm = (CardForm) findViewById(R.id.card_form);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(false)
+                .mobileNumberRequired(false)
+                .mobileNumberExplanation("SMS is required on this number")
+                .actionLabel("Purchase")
+                .setup(this);
     }
 
     @Override
