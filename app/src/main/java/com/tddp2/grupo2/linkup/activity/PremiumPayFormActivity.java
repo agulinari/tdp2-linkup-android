79322305e13694e1c8462312eadf067fde540053
Paula Saffioti <saffiotipaula@gmail.com>
2017-10-21 14:15:43 -0300

    validacion de datos de tarjetas de credito

--- app/src/main/java/com/tddp2/grupo2/linkup/activity/PremiumPayFormActivity.java
+++ app/src/main/java/com/tddp2/grupo2/linkup/activity/PremiumPayFormActivity.java
@@ -3,12 +3,16 @@ package com.tddp2.grupo2.linkup.activity;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.braintreepayments.cardform.utils.CardType;
+import com.braintreepayments.cardform.view.CardEditText;
 import com.braintreepayments.cardform.view.CardForm;
 import com.tddp2.grupo2.linkup.R;
 import com.tddp2.grupo2.linkup.activity.view.BaseView;
@@ -21,6 +25,11 @@ public class PremiumPayFormActivity extends BroadcastActivity implements BaseVie
 
     private PremiumPayFormController controller;
     private ProgressDialog progressDialog;
+    private CardForm cardForm;
+    private CardType cardType;
+
+    private static final String VISA = "VISA";
+    private static final String MASTERCARD = "MASTERCARD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,7 @@ public class PremiumPayFormActivity extends BroadcastActivity implements BaseVie
         setContentView(R.layout.activity_premium_pay_form);
         ButterKnife.bind(this);
 
-        CardForm cardForm = (CardForm) findViewById(R.id.card_form);
+        this.cardForm = (CardForm) findViewById(R.id.card_form);
         cardForm.cardRequired(true)
                 .expirationRequired(true)
                 .cvvRequired(true)
@@ -45,10 +54,23 @@ public class PremiumPayFormActivity extends BroadcastActivity implements BaseVie
     }
 
     private void registerListeners() {
+        cardForm.setOnCardTypeChangedListener(new CardEditText.OnCardTypeChangedListener() {
+            @Override
+            public void onCardTypeChanged(CardType card) {
+                cardType = card;
+            }
+        });
+
         buttonPayForm.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                controller.upgradeAccount();
+                if (!cardForm.isValid()) {
+                    showMissingFieldsAlert();
+                } else if (!cardType.name().equals(VISA) && !cardType.name().equals(MASTERCARD)) {
+                    showInvalidCardAlert();
+                } else {
+                    controller.upgradeAccount();
+                }
             }
         });
     }
@@ -82,5 +104,26 @@ public class PremiumPayFormActivity extends BroadcastActivity implements BaseVie
     @Override
     protected void handleNotification(Notification notification, BroadcastReceiver broadcastReceiver) {
         broadcastReceiver.abortBroadcast();
+    }
+
+    private void showMissingFieldsAlert() {
+        showErrorAlert(R.string.pay_missing_fields);
+    }
+
+    private void showInvalidCardAlert() {
+        showErrorAlert(R.string.pay_invalid_card);
+    }
+
+    private void showErrorAlert(int textId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(textId));
+        builder.setCancelable(Boolean.FALSE);
+        builder.setPositiveButton(getString(R.string.pop_up_ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
